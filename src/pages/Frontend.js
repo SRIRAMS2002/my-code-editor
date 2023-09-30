2d03e4c7db91ae9d5eb345e9e340b62db3ab4d2e
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-09-30 11:05:17 +0530

    frontend completed

--- src/pages/Frontend.js
+++ src/pages/Frontend.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from '@/Components/Navbar';
 import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/ext-language_tools'; // Import the language tools extension
+import 'ace-builds/src-noconflict/ext-language_tools';
 import 'ace-builds/src-noconflict/mode-html';
 import 'ace-builds/src-noconflict/mode-css';
 import 'ace-builds/src-noconflict/mode-javascript';
@@ -12,7 +12,6 @@ const Frontend = () => {
   const [cssCode, setCssCode] = useState('');
   const [jsCode, setJsCode] = useState('');
 
-  // Define snippets for each language
   const snippets = {
     html: [
       {
@@ -40,31 +39,25 @@ const Frontend = () => {
     ],
   };
 
-  // Initialize Ace Editor with snippet support
   useEffect(() => {
-    // Add snippets to the editor's snippet manager
     ace.require('ace/snippets').snippetManager.register(snippets.html, 'html');
     ace.require('ace/snippets').snippetManager.register(snippets.css, 'css');
     ace.require('ace/snippets').snippetManager.register(snippets.javascript, 'javascript');
   }, []);
 
   const runCode = () => {
-    // Execute JavaScript code
     try {
       const jsResult = eval(jsCode);
-      // Display JavaScript output (you can modify this to format as needed)
       console.log('JavaScript Output:', jsResult);
     } catch (jsError) {
       console.error('JavaScript Error:', jsError);
     }
 
-    // Create an iframe to render HTML and apply CSS
     const iframe = document.createElement('iframe');
     iframe.style.width = '100%';
     iframe.style.height = '100%';
     document.body.appendChild(iframe);
 
-    // Write HTML and CSS to the iframe
     const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
     iframeDocument.open();
     iframeDocument.write(htmlCode);
@@ -73,45 +66,65 @@ const Frontend = () => {
     iframeDocument.head.appendChild(style);
     iframeDocument.close();
   };
-
+ 
+  
   return (
-    <div>
-      <Navbar />
-      <div className='flex'>
-        <div>
-          <h1 className='font-bold text-center mx-auto'>HTML</h1>
+    <div >
+      <Navbar/>
+      <div className="bg-gray-100 mx-auto">
+      <div className='mt-6 grid grid-cols-1 gap-x-2 gap-y-10 sm:grid-cols-2 lg:grid-cols-3'>
+        <div className=' px-4 py-4'>
+          <h1 className='font-bold text-center mx-auto mb-4'>HTML</h1>
+         
           <AceEditor
             mode="html"
             theme="monokai"
             value={htmlCode}
             onChange={(newCode) => setHtmlCode(newCode)}
-            enableSnippets={true} // Enable snippet support for HTML editor
+            enableSnippets={true}
+            style={{ width: '100%', height: '300px' }  }
+           
           />
+          
+          
         </div>
-        <div>
-          <h1 className='font-bold text-center mx-auto'>CSS</h1>
+        <div className=' px-4 py-4'>
+          <h1 className='font-bold text-center mx-auto mb-4'>CSS</h1>
+         
           <AceEditor
             mode="css"
             theme="monokai"
             value={cssCode}
             onChange={(newCode) => setCssCode(newCode)}
-            enableSnippets={true} // Enable snippet support for CSS editor
+            enableSnippets={true}
+            style={{ width: '100%', height: '300px' }  }
           />
+          
+         
         </div>
-        <div>
-          <h1 className='font-bold text-center mx-auto'>JavaScript</h1>
+        <div className=' px-4 py-4'>
+          <h1 className='font-bold text-center mx-auto mb-4'>JavaScript</h1>
+         
           <AceEditor
             mode="javascript"
             theme="monokai"
             value={jsCode}
             onChange={(newCode) => setJsCode(newCode)}
-            enableSnippets={true} // Enable snippet support for JavaScript editor
+            enableSnippets={true}
+            style={{ width: '100%', height: '300px' }  }
           />
-          <button onClick={runCode}>Run</button>
-          {/* Output Display Area for JavaScript */}
-          {/* You can display JavaScript output here */}
+          
+         
+          
         </div>
       </div>
+      <button onClick={runCode} className='block mx-auto mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>
+            Run
+          </button>
+      </div>
+      <h1 className='block mx-auto mt-4 text-black font-bold py-2 px-4 rounded'>Output</h1>
+      
+    
     </div>
   );
 };
